12a0075ceee58d206f645d0617cbfeb897c2d007
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-01-19 23:36:08 +0800

    add function for cracking server

---
+++ src/projectA/model/crackServer.ts
@@ -0,0 +1,21 @@
+/** @param {NS} ns **/
+import { NS } from "@ns";
+
+export async function crack(ns: NS, target: string) {
+  if (ns.fileExists("BruteSSH.exe", "home")) {
+    ns.brutessh(target);
+  }
+  if (ns.fileExists("FTPCrack.exe", "home")) {
+    ns.ftpcrack(target);
+  }
+  if (ns.fileExists("relaySMTP.exe", "home")) {
+    ns.relaysmtp(target);
+  }
+  if (ns.fileExists("HTTPWorm.exe", "home")) {
+    ns.httpworm(target);
+  }
+  if (ns.fileExists("SQLInject.exe", "home")) {
+    ns.sqlinject(target);
+  }
+  ns.nuke(target);
+}
