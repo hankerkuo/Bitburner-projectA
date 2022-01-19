8119ab5ccb76cf6253e006bbd17176e05cf39206
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-01-19 23:34:46 +0800

    delete out-dated src files

--- src/model/hack.ts
+++
@@ -1,8 +0,0 @@
-/** @param {NS} ns **/
-
-import { updateKeyValue } from "./saveData";
-import { NS } from "@ns";
-
-// export async function hackOnFullMoney(ns: NS, target) {
-//   await ns.write(file, "{}", "w");
-// }
--- src/startGame.ts
+++
@@ -1,6 +0,0 @@
-/** @param {NS} ns **/
-import { NS } from "@ns";
-
-export async function main(ns: NS) {
-  
-}
