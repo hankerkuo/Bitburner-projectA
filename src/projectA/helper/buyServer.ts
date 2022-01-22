36ab21c8961062f65e69f9bfd0b70d2e2cd0d6bd
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-01-22 17:16:29 +0800

    add log for buy server

--- src/projectA/helper/buyServer.ts
+++ src/projectA/helper/buyServer.ts
@@ -3,6 +3,7 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS) {
+  ns.tprint(`Buying server, name: ${ns.args[0]}, core: ${ns.args[1]}`)
   ns.purchaseServer(
     ns.args[0].toString(),
     Math.pow(2, parseInt(ns.args[1].toString(), 10))
@@ -10,6 +11,7 @@ export async function main(ns: NS) {
 }
 
 export const buy = (ns: NS, name: string, core: number) => {
+  ns.tprint(`Buying server, name: ${name}, core: ${core}`)
   ns.purchaseServer(
     name,
     Math.pow(2, core)
