5afe10f689c4a6b672e4049b5cce62edf32c59b8
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-01-22 17:17:13 +0800

    add general start function

--- src/projectA/startGame.ts
+++ src/projectA/startGame.ts
@@ -5,28 +5,41 @@ import { runGeneralOnServer } from "/projectA/runGeneralOnServer";
 import { serverList } from "/projectA/serverInfo/serverList";
 
 export async function main(ns: NS) {
+  const distributeScript = async (serverName, portRequiredForTargets, ram) => {
+    const servers = serverList(portRequiredForTargets);
+    for (const targetServer in servers) {
+      if (!servers[targetServer]){
+        ns.tprint(`server: ${targetServer} is not profitable, skipping...`)
+        continue;
+      }
+      ns.tprint(
+        `start to run script on ${serverName}, using RAM: ${ram}, target server: ${targetServer}`
+      );
+      await runGeneralOnServer(
+        ns,
+        serverName,
+        `/projectA/generalHack.js`,
+        targetServer,
+        Math.pow(2, ram)
+      );
+    }
+  };
   // add 0 port server with 128GB
-  // const server0List = serverList(0);
-  // for (let i = 0; i < server0List.length; i++) {
-  //   const targetServer = server0List[i];
-  //   const ram = 7;
-  //   ns.tprint(`start to run script on home, using RAM: ${ram}, target server: ${targetServer}`)
-  //   await runGeneralOnServer(ns, "home", `/projectA/generalHack.js`, targetServer, Math.pow(2, ram));
-  // }
+  // await distributeScript("home", 0, 7);
+
   // add 1 port server with 256GB
-  const server0List = serverList(1);
-  for (let i = 0; i < server0List.length; i++) {
-    const targetServer = server0List[i];
-    const ram = 8;
-    ns.tprint(`start to run script on home, using RAM: ${ram}, target server: ${targetServer}`)
-    await runGeneralOnServer(ns, "home", `/projectA/generalHack.js`, targetServer, Math.pow(2, ram));
+  // await distributeScript("home", 1, 8);
+
+  // add 2 port server with 1024GB
+  // await distributeScript("home", 2, 10);
+
+  // add 3 port server with 2048GB
+  // await distributeScript("home", 3, 11);
+
+  for (let i = 0; i < 10; i++) {
+    buy(ns, `ForPort4-${i}`, 15);
+    await distributeScript(`ForPort4-${i}`, 4, 11);
   }
-  //TODO: add other port servers
-  //TODO: add buy server section
-  // ns.tprint("start to buy servers");
-  // buy(ns, newServerName, ram);
-  // ns.tprint(`start to run script on ${newServerName}, RAM: ${ram}`)
-  // await runGeneralOnServer(ns, newServerName, "/projectA/generalHack.js", server0List[i], 0);
 
   // ns.deleteServer("256GB-0");
 }
