63fae24c588a2ecc5638fcb5111e2b2dc97680c4
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-01-22 17:15:04 +0800

    add port opened detection in crackServer

--- src/projectA/model/crackServer.ts
+++ src/projectA/model/crackServer.ts
@@ -2,20 +2,33 @@
 import { NS } from "@ns";
 
 export async function crack(ns: NS, target: string) {
+  let portOpened = 0;
   if (ns.fileExists("BruteSSH.exe", "home")) {
     ns.brutessh(target);
+    portOpened ++;
   }
   if (ns.fileExists("FTPCrack.exe", "home")) {
     ns.ftpcrack(target);
+    portOpened ++;
   }
   if (ns.fileExists("relaySMTP.exe", "home")) {
     ns.relaysmtp(target);
+    portOpened ++;
   }
   if (ns.fileExists("HTTPWorm.exe", "home")) {
     ns.httpworm(target);
+    portOpened ++;
   }
   if (ns.fileExists("SQLInject.exe", "home")) {
     ns.sqlinject(target);
+    portOpened ++;
   }
-  ns.nuke(target);
+  if (portOpened >= ns.getServerNumPortsRequired(target)){
+    ns.nuke(target);
+  }
+  else{
+    throw new Error(`Cannot open enough ports for server NUKE: ${target},
+    port needed: ${ns.getServerNumPortsRequired(target)},
+    port opend: ${portOpened}`)
+  }
 }
