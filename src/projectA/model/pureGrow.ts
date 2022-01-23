782a8c7a8e3fd1d2cd7f6e0692f8e652a3623701
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-01-23 22:24:04 +0800

    pure function running on side-servers

--- src/projectA/model/pureGrow.ts
+++ src/projectA/model/pureGrow.ts
@@ -3,16 +3,6 @@
 import { NS } from "@ns";
 
 export async function main(ns:NS) {
-  var target = ns.args[0].toString();
-  var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-
-  const isMoneyQuataEnough = () => {
-    return ns.getServerMoneyAvailable(target) < moneyThresh;
-  };
-  while (true) {
-    if (isMoneyQuataEnough()) {
-      await ns.grow(target);
-    }
-    await ns.sleep(1000);
-  }
+  const target = ns.args[0].toString();
+  await ns.grow(target);
 }
