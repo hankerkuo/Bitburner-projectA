e5aa27e2e8d0bf2b9c12db0ebffd2549e95f445a
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-02-03 01:47:48 +0800

    remove useless functions in worker

--- src/projectA/model/worker.ts
+++ src/projectA/model/worker.ts
@@ -8,6 +8,7 @@ import {
   growThread,
   weakenThreadForHack,
   weakenThreadForGrow,
+  weakenToLowest
 } from "/projectA/model/threadCalculate";
 import { scriptPosition } from "/projectA/serverInfo/ScriptInfo";
 
@@ -16,17 +17,13 @@ interface Worker {
   observedServer: string;
   timeLine: TimeLine;
   hackRatio: number;
+  hackTime: number;
   growTime: number;
   weakTime: number;
-  minThreadGrowNeed: number;
   hackThread(): number;
-  hackTiming(): number;
   weak1Thread(): number;
-  weak1Timing(): number;
   growThread(): number;
-  growTiming(): number;
   weak2Thread(): number;
-  weak2Timing(): number;
   runHack(runOn: string): void;
   runGrow(runOn: string): void;
   runWeak1(runOn: string): void;
@@ -39,46 +36,30 @@ export class BaseWorker implements Worker {
   public observedServer;
   public timeLine;
   public hackRatio;
+  public hackTime;
   public growTime;
   public weakTime;
-  public minThreadGrowNeed;
   private threadThresh: number = Math.pow(2, 15);
   constructor(ns: NS, observedServer: string, hackRatio: number) {
     this.ns = ns;
     this.observedServer = observedServer;
-    let hackTime = ns.getHackTime(observedServer);
+    this.hackTime = ns.getHackTime(observedServer);
     this.growTime = ns.getGrowTime(observedServer);
     this.weakTime = ns.getWeakenTime(observedServer);
-    this.timeLine = calculateTimeLine(hackTime, this.growTime, this.weakTime);
+    this.timeLine = calculateTimeLine(this.hackTime, this.growTime, this.weakTime);
     this.hackRatio = hackRatio;
-    this.minThreadGrowNeed = ns.growthAnalyze(
-      observedServer,
-      1 / (1 - hackRatio)
-    );
   }
   public growThread(): number {
     return 1.2 * growThread(this.ns, this.observedServer, this.hackRatio);
   }
-  public growTiming(): number {
-    return this.timeLine.growStart;
-  }
   public hackThread(): number {
     return hackThread(this.ns, this.observedServer, this.hackRatio);
-  }
-  public hackTiming(): number {
-    return this.timeLine.hackStart;
   }
   public weak1Thread(): number {
     return 1.2 * weakenThreadForHack(this.ns, this.observedServer, this.hackRatio);
   }
-  public weak1Timing(): number {
-    return this.timeLine.firstWeakStart;
-  }
   public weak2Thread(): number {
     return 1.2 * weakenThreadForGrow(this.ns, this.observedServer, this.hackRatio);
-  }
-  public weak2Timing(): number {
-    return this.timeLine.secondWeakStart;
   }
   public runGrow(runOn: string): void {
     const thread = this.growThread();
@@ -125,14 +106,11 @@ export class BaseWorker implements Worker {
     );
   }
   public runWeakToLowest(runOn: string): void {
-    const thread =
-      (this.ns.getServerSecurityLevel(this.observedServer) -
-      this.ns.getServerMinSecurityLevel(this.observedServer)) /
-      0.05;
+    const thread = 1.1 * weakenToLowest(this.ns, this.observedServer);
     this.ns.exec(
       scriptPosition.pureWeaken,
       runOn,
-      thread * 1.1,
+      thread,
       this.observedServer,
       Date.now()
     );
