9b5fc5c3119113e0be9ec218717f57392c6a1005
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-02-05 13:05:10 +0800

    jest config for typescript module path

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // !!This is important for jest module mapping!!
+  moduleNameMapper: {
+    '^/(.*)$': '<rootDir>/src/$1',
+  },
 };
