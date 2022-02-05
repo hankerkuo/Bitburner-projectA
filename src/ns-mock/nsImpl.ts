313dc9eb56a4fc3096e3935da3fe972e0c1f8760
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-02-06 00:47:57 +0800

    code format for nsImpl

--- src/ns-mock/nsImpl.ts
+++ src/ns-mock/nsImpl.ts
@@ -1,9 +1,12 @@
-import { NSMock, BasicHGWOptions } from '@ns-mock'
-import { TestConst } from '/ns-mock/constant/const';
+import {
+  NSMock,
+  BasicHGWOptions,
+} from "/ns-mock/interface/NetscriptDefinitions";
+import { TestConst } from "/ns-mock/constant/const";
 
 export class NsImpl implements NSMock {
   readonly args;
-  constructor(args0:string){
+  constructor(args0: string) {
     this.args = [args0];
   }
   hack(host: string, opts?: BasicHGWOptions): Promise<number> {
@@ -11,13 +14,13 @@ export class NsImpl implements NSMock {
       // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
       // In this example, we use setTimeout(...) to simulate async code.
       // In reality, you will probably be using something like XHR or an HTML5 API.
-      setTimeout( function() {
-        if (host === "testServer"){
-          resolve(TestConst.HACK_AMOUNT_TEST_SERVER)
-        } else{
-          resolve(0)
+      setTimeout(function () {
+        if (host === "testServer") {
+          resolve(TestConst.HACK_AMOUNT_TEST_SERVER);
+        } else {
+          resolve(0);
         }
-      }, 250)
-    })
+      }, 250);
+    });
   }
 }
