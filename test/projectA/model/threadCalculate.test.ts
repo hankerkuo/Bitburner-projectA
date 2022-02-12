fc4085fa45f39c0f15f61685b5071092296eebb0
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-02-12 09:35:06 +0800

    add more test for thread calculation

--- test/projectA/model/threadCalculate.test.ts
+++ test/projectA/model/threadCalculate.test.ts
@@ -1,4 +1,10 @@
-import { hackThread, growThread, weakenThreadForHack } from "../../../src/projectA/model/threadCalculate";
+import {
+  hackThread,
+  growThread,
+  weakenThreadForHack,
+  weakenThreadForGrow,
+  weakenToLowest,
+} from "../../../src/projectA/model/threadCalculate";
 import { CONST } from "../../../src/projectA/serverInfo/constant";
 import { NsImpl } from "../../../src/ns-mock/nsImpl";
 import { TestConst } from "../../../src/ns-mock/constant/const";
@@ -10,18 +16,14 @@ describe("Hack thread calculate verification", () => {
     const threadCalculated = hackThread(
       ns as NS,
       "testServer",
-      0.99 * ns.getServerMoneyAvailable("testServer") /
+      (0.99 * ns.getServerMoneyAvailable("testServer")) /
         ns.getServerMaxMoney("testServer")
     );
     expect(threadCalculated).toBeGreaterThan(0);
     expect(threadCalculated).toBeLessThanOrEqual(CONST.MAX_THREAD_PER_SCRIPT);
   });
   it("Check hack thread result return -1 when hack ratio > 1", () => {
-    const threadCalculated = hackThread(
-      ns as NS,
-      "testServer",
-      1.1
-    );
+    const threadCalculated = hackThread(ns as NS, "testServer", 1.1);
     expect(threadCalculated).toBe(-1);
   });
   it("Check hack thread result return -1 when server not exists", () => {
@@ -36,7 +38,7 @@ describe("Hack thread calculate verification", () => {
     const threadCalculated = hackThread(
       ns as NS,
       "testServer",
-      1.1 * ns.getServerMoneyAvailable("testServer") /
+      (1.1 * ns.getServerMoneyAvailable("testServer")) /
         ns.getServerMaxMoney("testServer")
     );
     expect(threadCalculated).toBe(-1);
@@ -75,7 +77,7 @@ describe("Grow thread calculate verification", () => {
 
 describe("Weaken Thread For Hack calculate verification", () => {
   const ns = new NsImpl("");
-  it("Check grow thread result not greater than max thread per script", () => {
+  it("Check thread result not greater than max thread per script", () => {
     const threadCalculated = weakenThreadForHack(
       ns as NS,
       "testServer",
@@ -85,7 +87,7 @@ describe("Weaken Thread For Hack calculate verification", () => {
     expect(threadCalculated).toBeGreaterThan(0);
     expect(threadCalculated).toBeLessThanOrEqual(CONST.MAX_THREAD_PER_SCRIPT);
   });
-  it("Check grow thread result return -1 when server not exists", () => {
+  it("Check thread result return -1 when server not exists", () => {
     const threadCalculated = weakenThreadForHack(
       ns as NS,
       "nonExistServer",
@@ -94,14 +96,55 @@ describe("Weaken Thread For Hack calculate verification", () => {
     );
     expect(threadCalculated).toBe(-1);
   });
-  it("Check grow thread result return -1 when server money is not enough", () => {
+  it("Check thread result return -1 when server money is not enough", () => {
     const threadCalculated = weakenThreadForHack(
       ns as NS,
       "nonExistServer",
-      1.1 * ns.getServerMoneyAvailable("testServer") /
+      (1.1 * ns.getServerMoneyAvailable("testServer")) /
         ns.getServerMaxMoney("testServer")
     );
     expect(threadCalculated).toBe(-1);
-  })
+  });
 });
 
+describe("Weaken Thread For Grow calculate verification", () => {
+  const ns = new NsImpl("");
+  it("Check thread result not greater than max thread per script", () => {
+    const threadCalculated = weakenThreadForGrow(
+      ns as NS,
+      "testServer",
+      ns.getServerMoneyAvailable("testServer") /
+        ns.getServerMaxMoney("testServer")
+    );
+    expect(threadCalculated).toBeGreaterThan(0);
+    expect(threadCalculated).toBeLessThanOrEqual(CONST.MAX_THREAD_PER_SCRIPT);
+  });
+  it("Check thread result return -1 when server not exists", () => {
+    const threadCalculated = weakenThreadForGrow(
+      ns as NS,
+      "nonExistServer",
+      ns.getServerMoneyAvailable("testServer") /
+        ns.getServerMaxMoney("testServer")
+    );
+    expect(threadCalculated).toBe(-1);
+  });
+});
+
+describe("Weaken to lowest calculate verification", () => {
+  const ns = new NsImpl("");
+  it("Check thread result not greater than max thread per script", () => {
+    const threadCalculated = weakenToLowest(
+      ns as NS,
+      "testServer"
+    );
+    expect(threadCalculated).toBeGreaterThan(0);
+    expect(threadCalculated).toBeLessThanOrEqual(CONST.MAX_THREAD_PER_SCRIPT);
+  });
+  it("Check thread return 0 when server is already at minimum security level", () => {
+    const threadCalculated = weakenToLowest(
+      ns as NS,
+      "testServer_minSec"
+    );
+    expect(threadCalculated).toBe(0);
+  })
+});
