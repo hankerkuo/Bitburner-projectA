084e3c2c6210e4da5e149722d40f9c36c6d4bddb
LAPTOP-FOSMIQ0B\Hank Kuo <hankerkuo@gmail.com>
2022-03-17 22:49:36 +0800

    constant update

--- src/projectA/serverInfo/constant.ts
+++ src/projectA/serverInfo/constant.ts
@@ -1,21 +1,21 @@
 export const CONST = {
   // this will limit the thread calculation model's max
   // value calculated for running scripts
-  MAX_THREAD_PER_SCRIPT: 200,
+  MAX_THREAD_PER_SCRIPT: 100,
 
   // this will be used to gate the `grow` script
   // since grow script is likely to use lots of theads
-  WAIT_THRESHOLD_THREAD: 300,
+  WAIT_THRESHOLD_THREAD: 100,
 
   // Action series: Hack->Weak->Grow->Weak
   // this constant define the interval between them
   // the smaller, the more RAM needed!
-  ACTION_INTERVAL: 1000,
+  ACTION_INTERVAL: 800,
   
   // the percentage of `MAX money` aiming to hack
   // within a single run, in the early game with
   // limited RAM, you may need to set this low
-  HACK_RATIO: 0.3,
+  HACK_RATIO: 0.1,
   
   // set to 0 will let the grow ratio automatic
   // automatic means the script will try to 
